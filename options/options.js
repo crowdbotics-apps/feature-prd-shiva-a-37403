df2c77fa229995b653b8e0dafa0bb704a6a01a98
Crowdbotics <team@crowdbotics.com>
2022-10-27 13:47:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "feature_prd_shiva_a_37403",
+  url: "https://feature_prd_shiva_a_37403.botics.co",
+  api: "https://feature_prd_shiva_a_37403.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
